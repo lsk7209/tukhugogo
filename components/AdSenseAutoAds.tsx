6b16c2c74291278b9184805f6209c6f2ba51e81b
lsk7209 <lsk7209@gmail.com>
2026-07-29 10:38:53 +0900

    Scope ads to reader detail pages

--- components/AdSenseAutoAds.tsx
+++ components/AdSenseAutoAds.tsx
@@ -1,12 +1,22 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Script from "next/script";
+
+const isReaderDetailRoute = (pathname: string) =>
+  /^\/(guide|tech|ranking|blog)\/[^/]+\/?$/.test(pathname);
+
 export function AdSenseAutoAds({ publisherId }: { publisherId: string }) {
-  if (!publisherId) return null;
+  const pathname = usePathname();
+
+  if (!publisherId || !isReaderDetailRoute(pathname)) return null;
 
   return (
-    <script
-      id="adsense-loader"
+    <Script
       async
       crossOrigin="anonymous"
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${publisherId}`}
+      strategy="afterInteractive"
     />
   );
 }
